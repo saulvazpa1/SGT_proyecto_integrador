2ab92e5ddd86fe2b08ac39a44f0c0f4d2fea5e8c
umoralesroman57-cmyk <umoralesroman57@gmail.com>
2026-07-10 11:25:05 -0600

    Erees bot

--- dao/usuario_dao.py
+++ dao/usuario_dao.py
@@ -1,19 +1,23 @@
 #DAO: Data Access Object
 #usuario_dao: Objeto de acceso a datos de la tabla usuarios
 
-from database.conexion import Conexion
+
+from database.conexion import Conexion    #Carpeta/archivo/clase 
 from models.usuario import Usuario
 
 class UsuarioDAO:
-    # SELECT * FROM usuarios
+#SELECT * FROM usuarios
     def obtener_todos(self):
         conexion = Conexion.obtener_conexion()
+        #objeto para postgresql 
         cursor = conexion.cursor()
+        #que comando ejecutar
+
         cursor.execute("SELECT * FROM usuarios")
-        registros = cursor.fetchall()
+        registros = cursor.fetchall() #nombre de la tabla/el resultado de cursor fetchall
         
-        usuarios = []
-        for registro in registros:
+        usuarios = [] #lista vacia
+        for registro in registros:  #crea un nuevo usuario con nueva info
             usuario = Usuario(
                 usuario_id=registro[0],
                 usuario_nombre=registro[1],
@@ -25,15 +29,17 @@ class UsuarioDAO:
                 rol_id=registro[7]
             )
             usuarios.append(usuario)
-            
+
         cursor.close()
         conexion.close()
         return usuarios
+    #Crear insertar
 
-    # Crear insertar
     def insertar(self, usuario):
         conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
+        cursor = conexion.cursor()#Conexion es la clase conexion es la variable
+
+#recibe parametro %s
         sql = """
         INSERT INTO usuarios(usuario_id, usuario_nombre, usuario_apellidop, usuario_apellidom, usuario_telefono, usuario_correo, usuario_password, rol_id)
         VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
@@ -49,19 +55,23 @@ class UsuarioDAO:
             usuario.usuario_password,
             usuario.rol_id)
         )
+
         conexion.commit()
         cursor.close()
         conexion.close()
+        
 
     def actualizar(self, usuario):
         conexion = Conexion.obtener_conexion()
-        cursor = conexion.cursor()
-        sql = """
+        cursor = conexion.cursor()#C
+
+        #modifique por ID 
+        sql = """ 
         UPDATE usuarios
         SET usuario_nombre = %s, usuario_apellidop = %s, usuario_apellidom = %s,
             usuario_telefono = %s, usuario_correo = %s, usuario_password = %s,
             rol_id = %s
-        WHERE usuario_id = %s
+        WHERE usuario_id = %s  
         """
         cursor.execute(
             sql,
@@ -74,18 +84,35 @@ class UsuarioDAO:
             usuario.rol_id,
             usuario.usuario_id)
         )
+
+        conexion.commit()#confirmar un edicion de base de datos
+        cursor.close()
+        conexion.close()
+
+
+
+    #eliminar un regsitro
+    def eliminar(self, usuario_id):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()#C
+
+        cursor.execute(
+            "DELETE FROM usuarios WHERE usuario_id = %s",
+             (usuario_id,)
+             )
         conexion.commit()
         cursor.close()
         conexion.close()
 
-    # eliminar un registro
-    def eliminar(self, usuario_id):
+    def obtener_ultimo_id(self):
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
-        cursor.execute(
-            "DELETE FROM usuarios WHERE usuario_id = %s",
-            (usuario_id,)
-        )
-        conexion.commit()
+
+        cursor.execute("SELECT usuario_id FROM usuarios ORDER BY usuario_id DESC")
+        resultado = cursor.fetchone()
         cursor.close()
         conexion.close()
+
+        if resultado is None:
+            return 0
+        return resultado[0]
